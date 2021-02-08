ed3131e995dc277727eeeea837c68bb0407133b9
Qizhang Jia <qj3fe@virginia.edu>
2021-02-08 13:28:54 -0800

    210208_prot

---
+++ Rosalind/stronghold/PROT.py
@@ -0,0 +1,37 @@
+###
+# @Author: Qizhang Jia
+# @Date: 2021-02-08 13:18:03
+# @Last Modified by:   Qizhang Jia
+# @Last Modified time: 2021-02-08 13:18:03
+###
+
+codon_table = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G',
+}
+with open('Rosalind/stronghold/input_files/rosalind_prot.txt', 'r') as f:
+    RNA_string = f.readline()
+
+protein_string = []
+for j in range(len(RNA_string) // 3):
+    codon = codon_table[RNA_string[3 * j:3 * (j + 1)]]
+    if codon == 'Stop':
+        break
+    else:
+        protein_string.append(codon)
+
+print(''.join(protein_string))
