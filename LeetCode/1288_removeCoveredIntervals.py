afc194d90171b7a2cbd004602a9d3dd5880514b8
Qizhang Jia <qj3fe@virginia.edu>
2021-03-16 23:04:52 -0700

    0316

---
+++ LeetCode/1288_removeCoveredIntervals.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        
+        """
+            Runtime: 88 ms, faster than 94.21% of Python3 online submissions for Remove Covered Intervals.
+            Memory Usage: 14.8 MB, less than 55.14% of Python3 online submissions for Remove Covered Intervals.
+        """
+        
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        
+        count = 0
+        
+        s0, e0 = float("-inf"), float("-inf")
+        for start, end in intervals:
+            if start >= s0 and end <= e0:
+                continue
+            else:
+                count += 1
+                s0, e0 = start, end
+                
+        return count
---
+++ LeetCode/435_eraseOverlapIntervals.py
@@ -0,0 +1,21 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+        """
+            Runtime: 76 ms, faster than 31.03% of Python3 online submissions for Non-overlapping Intervals.
+            Memory Usage: 17.5 MB, less than 13.11% of Python3 online submissions for Non-overlapping Intervals.
+        """
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        prev = float("-inf")
+        count = 0
+        for start, end in intervals:
+            if start >= prev:
+                prev = end
+                continue
+            else:
+                prev = min(prev, end)
+                count += 1
+        
+        return count
